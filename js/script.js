4e197824473bd783dc51c28aa4531787e542b008
Saidov2211 <77971652+Alexander170@users.noreply.github.com>
2023-12-19 20:36:56 +0300

    speed change

--- js/script.js
+++ js/script.js
@@ -1,31 +1,29 @@
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
-
 const ground = new Image();
 ground.src = "img/snake_pole.png";
-
 const foodImg = new Image();
 foodImg.src = "img/food.png";
-
 let box = 32;
 
 let score = 0;
+
+let startSpeed = 200;
+let currentSpeed = startSpeed;
 
 let food = {
   x: Math.floor(Math.random() * 17 + 1) * box,
   y: Math.floor(Math.random() * 15 + 3) * box,
 };
-
 let snake = [];
 snake[0] = {
   x: 9 * box,
   y: 10 * box,
 };
-
+let snakeX = snake[0].x;
+let snakeY = snake[0].y;
 document.addEventListener("keydown", direction);
-
 let dir;
-
 function direction(event) {
   if (event.keyCode == 37 && dir != "right") dir = "left";
   else if (event.keyCode == 38 && dir != "down") dir = "up";
@@ -34,8 +32,8 @@ function direction(event) {
 }
 
 function eatTail(head, arr) {
-  for (let i = 0; i < arr.length; i++) {
-    if (head.x == arr[i].x && head.y == arr[i].y) clearInterval(game);
+  for (let i = 1; i < arr.length; i++) {
+    if (head.x == arr[i].x && head.y == arr[i].y) gameOver();
   }
 }
 
@@ -49,11 +47,19 @@ function drawGame() {
   ctx.fillStyle = "white";
   ctx.font = "50px Arial";
   ctx.fillText(score, box * 2.5, box * 1.7);
-  let snakeX = snake[0].x;
-  let snakeY = snake[0].y;
+  ctx.fillText(
+    "Speed " + (1000 / currentSpeed).toFixed(1),
+    box * 10.5,
+    box * 1.7
+  );
 
   if (snakeX == food.x && snakeY == food.y) {
     score++;
+    if (currentSpeed >= startSpeed * 0.3) {
+      currentSpeed -= startSpeed * 0.02;
+      clearInterval(game);
+      game = setInterval(drawGame, currentSpeed);
+    }
     food = {
       x: Math.floor(Math.random() * 17 + 1) * box,
       y: Math.floor(Math.random() * 15 + 3) * box,
@@ -61,28 +67,43 @@ function drawGame() {
   } else {
     snake.pop();
   }
-
   if (
     snakeX < box ||
     snakeX > box * 17 ||
     snakeY < 3 * box ||
     snakeY > box * 17
-  )
+  ) {
     clearInterval(game);
-
+    gameOver();
+  }
   if (dir == "left") snakeX -= box;
   if (dir == "right") snakeX += box;
   if (dir == "up") snakeY -= box;
   if (dir == "down") snakeY += box;
-
   let newHead = {
     x: snakeX,
     y: snakeY,
   };
-
   eatTail(newHead, snake);
-
   snake.unshift(newHead);
 }
+function gameOver() {
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = "red";
+  ctx.font = "40px Arial";
+  ctx.fillText("Вы проиграли", box * 6, box * 10);
+  ctx.font = "20px Arial";
+  ctx.fillText("Нажмите Enter, чтобы перезапустить игру", box * 4, box * 12);
+  document.removeEventListener("keydown", direction);
+  document.addEventListener("keydown", restartGame);
+}
 
-let game = setInterval(drawGame, 100);
+function restartGame(event) {
+  if (event.keyCode == 13) {
+    currentSpeed = startSpeed;
+    location.reload();
+  }
+}
+
+let game = setInterval(drawGame, currentSpeed);
